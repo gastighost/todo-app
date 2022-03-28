bb6ba360163e0be2004a384903c7c7a4191bba4f
Gaston Roxas <gacaro51597@gmail.com>
2022-03-28 22:17:54 +0200

    added setup files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
+import React, { useContext } from 'react';
 import './App.css';
+import { UserContext } from './index';
 
 function App() {
+  const value = useContext(UserContext)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Received, {value}
     </div>
   );
 }
