c70fae5b3a34d6da90ded9147219bf5e198dddc7
Gaston Roxas <gacaro51597@gmail.com>
2022-03-31 19:37:30 +0800

    add create, edit and delete functionality

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
 import React, { useReducer } from 'react';
-import './App.css';
-import { Button } from 'react-bootstrap';
+import ToDoList from './ToDoList';
+import { v4 as uuidv4 } from 'uuid';
 
-const initialState = {
-  count: 0
-}
+const todosInitialState = {
+  todos:[{id: 1, text: "finishing writing hooks chapter"},
+         {id: 2, text: "play with kids"},
+         {id: 3, text: "read bible"}]
+};
+
+export const TodosContext = React.createContext();
 
 function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useReducer(todosReducer, todosInitialState);
   return (
-    <div>
-      Count: {state.count}
-      <br/>
-      <Button onClick={() => dispatch({type: "increment"})}>
-        Increment
-      </Button>
-      <Button variant="secondary" onClick={() => dispatch({type: "decrement"})}>
-        Decrement
-      </Button>
-      <Button variant="success" onClick={() => dispatch({type: "reset"})}>
-        Reset
-      </Button>
-    </div>
+    <TodosContext.Provider value={{state, dispatch}}>
+      <ToDoList />
+    </TodosContext.Provider>
   )
 }
 
-function reducer(state, action) {
+function todosReducer(state, action) {
   switch(action.type) {
-    case "increment":
-      return {count: state.count + 1}
-    case "decrement":
-      return {count: state.count - 1}
-    case "reset":
-      return initialState
+    case 'add':
+      const newToDo = {id: uuidv4(), text:action.payload}
+      const addedToDos = [...state.todos, newToDo]
+      return {...state, todos: addedToDos}
+    case 'edit':
+      const updatedToDo = {...action.payload};
+      const updatedToDoIndex = state.todos.findIndex(t => t.id === action.payload.id);
+      const updatedToDos = [
+        ...state.todos.slice(0, updatedToDoIndex),
+        updatedToDo,
+        ...state.todos.slice(updatedToDoIndex + 1)
+      ];
+      return {...state, todos: updatedToDos}
+    case 'delete':
+      const filteredTodoState = state.todos.filter(todo => todo.id !== action.payload.id)
+      return {...state, todos: filteredTodoState}
     default:
-      return initialState
+      return todosInitialState
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-export const UserContext = React.createContext();
-const username = "Greg"
-
 ReactDOM.render(
   <React.StrictMode>
-    <UserContext.Provider value={username}>
-      <App />
-    </UserContext.Provider>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
