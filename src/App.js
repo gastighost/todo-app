cbfe6d6295fbd13ddec9e602401d2bf482f73d6c
Gaston Roxas <gacaro51597@gmail.com>
2022-03-31 17:28:37 +0800

    add counter feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React, { useContext } from 'react';
+import React, { useReducer } from 'react';
 import './App.css';
-import { UserContext } from './index';
+import { Button } from 'react-bootstrap';
+
+const initialState = {
+  count: 0
+}
 
 function App() {
-  const value = useContext(UserContext)
+  const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div className="App">
-      Received, {value}
+    <div>
+      Count: {state.count}
+      <br/>
+      <Button onClick={() => dispatch({type: "increment"})}>
+        Increment
+      </Button>
+      <Button variant="secondary" onClick={() => dispatch({type: "decrement"})}>
+        Decrement
+      </Button>
+      <Button variant="success" onClick={() => dispatch({type: "reset"})}>
+        Reset
+      </Button>
     </div>
-  );
+  )
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "increment":
+      return {count: state.count + 1}
+    case "decrement":
+      return {count: state.count - 1}
+    case "reset":
+      return initialState
+    default:
+      return initialState
+  }
 }
 
 export default App;
